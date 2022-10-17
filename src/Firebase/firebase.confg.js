0655df61ccb603c070c6ef6eb6a3f11abe3908ec
arefinnuman <arefinnuman2000@gmail.com>
2022-10-18 00:02:29 +0600

    Good Night

---
+++ src/Firebase/firebase.confg.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnvzhVv7pxZJdsqi-XlaANbP4b5Xurxv0",
+  authDomain: "love-news-c44f4.firebaseapp.com",
+  projectId: "love-news-c44f4",
+  storageBucket: "love-news-c44f4.appspot.com",
+  messagingSenderId: "665212100281",
+  appId: "1:665212100281:web:c72c4d00a7b85529664568",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/Routes/Public/Routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../../Layout/Root";
+import Home from "../../Pages/Home/Home";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [{ path: "/", element: <Home></Home> }],
+  },
+]);
