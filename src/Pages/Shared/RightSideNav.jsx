ae8f761efeb09b750f440c6a2cd752350404d832
arefinnuman <arefinnuman2000@gmail.com>
2022-10-18 22:53:27 +0600

    Commit

--- src/Pages/Shared/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav.jsx
@@ -1,9 +1,49 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import ListGroup from "react-bootstrap/ListGroup";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaTwitch,
+  FaTwitter,
+} from "react-icons/fa";
+import BrandCarousel from "./BrandCarousel";
 
 const RightSideNav = () => {
   return (
     <div>
-      <h1>Right Side Nav</h1>
+      <ButtonGroup className="mb-2" vertical>
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle className="m-2"></FaGoogle> Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          <FaGithub className="m-2"></FaGithub> Login with Github
+        </Button>
+      </ButtonGroup>
+      <div>
+        <h4>Find us on</h4>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook className="m-2"></FaFacebook>Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaGithub className="m-2"></FaGithub>Github
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaInstagram className="m-2"></FaInstagram> Instagram
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter className="m-2"></FaTwitter>Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitch className="m-2"></FaTwitch>Twitch
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <BrandCarousel></BrandCarousel>
     </div>
   );
 };
--- src/Routes/Public/Routes.jsx
+++ src/Routes/Public/Routes.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../../Layout/Root";
+import Category from "../../Pages/Category/Category";
 import Home from "../../Pages/Home/Home";
+import News from "../../Pages/News/News";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    children: [{ path: "/", element: <Home></Home> }],
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "/category/:id", element: <Category></Category> },
+      { path: "news/:id", element: <News></News> },
+    ],
   },
 ]);
